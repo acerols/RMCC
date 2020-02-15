dec938fa5c2137df0dff5475cb83362839964639
Yuto Kaihara <arizexy@gmail.com>
2020-02-15 04:50:10 +0000

    modified:   rmcc.c 	modified:   test.sh

--- rmcc.c
+++ rmcc.c
@@ -3,226 +3,244 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "rmcc.h"
+#include <stdbool.h>
 
-Token tokens[100];
+typedef enum{
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+}NodeKind;
+
+typedef struct Node Node;
+
+struct Node{
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+typedef enum {
+	TK_RESERVED,
+	TK_NUM,
+	TK_EOF,
+}TokenKind;
+
+typedef struct Token Token;
+
+struct Token{
+	TokenKind kind;
+	Token *next;
+	int val;
+	char *str;
+};
+
+Token *token;
 int pos = 0;
 
-Node *add();
-Node *mul();
-Node *term();
-int consume(int ty);
-
-Vector *new_vector();
-void vec_push(Vector *vec, void *elem);
-
-void expect(int line, int expected, int actual)
-{
-	if(expected == actual)
-		return;
-	fprintf(stderr, "%d: %d expected, but got %d\n",
-			line, expected, actual);
-	exit(1);
-}
-
-void runtest()
-{
-	Vector *vec = new_vector();
-	expect(__LINE__, 0, vec->len);
-
-	for(int i = 0; i < 100; i++)
-		vec_push(vec, (void *)i);
-	
-	expect(__LINE__, 100, vec->len);
-	expect(__LINE__, 0, (int)vec->data[0]);
-	expect(__LINE__, 50, (int)vec->data[50]);
-	expect(__LINE__, 99, (int)vec->data[99]);
-
-	printf("OK\n");
-}
-
-Vector *new_vector()
-{
-	
-	Vector *vec = malloc(sizeof(Vector));
-	vec->data = malloc(sizeof(void *) * 16);
-	vec->capacity = 16;
-	vec->len = 0;
-	return vec;
-}
-
-void vec_push(Vector *vec, void *elem)
-{
-	if(vec->capacity == vec->len){
-		vec->capacity *=2;
-		vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
-	}
-	vec->data[vec->len++] = elem;
-}
-
-void tokennize(char *p)
-{
-	int i = 0;
-	while(*p){
-		if(isspace(*p)){
-			p++;
-			continue;
-		}
-		
-		if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
-			tokens[i].ty = *p;
-			tokens[i].input = p;
-			i++;
-			p++;
-			continue;
-		}
-
-		if(isdigit(*p)){
-			tokens[i].ty = TK_NUM;
-			tokens[i].input = p;
-			tokens[i].val = strtol(p, &p, 10);
-			i++;
-			continue;
-		}
-		fprintf(stderr, "トークナイズできません : %s\n", p);
-		exit(1);
-	}
-
-	tokens[i].ty = TK_EOF;
-	tokens[i].input = p;
-}
-/*
-	int j;
-	for(j = 0; j <= i; j++){
-		printf("%d\n", tokens[j].ty);
-	}
-	
-}
-*/
-
-void error(const char *format, ...)
+//エラーを報告関数
+//printfと同じ引数
+void error(char *fmt, ...)
 {
 	va_list ap;
-	va_start(ap, format);
-	vprintf(format, ap);
-	va_end(ap);
-	exit(1);
-	/*fprintf(stderr, "予期せぬトークンです : %s\n",
-		tokens[i].input);
-	*/
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
 	exit(1);
 }
 
-
-void print_tk(Token tks)
+char *user_input;
+void error_at(char *loc, char *fmt, ...)
 {
-	if(tks.ty == TK_NUM){
-		printf("%d", tks.val);
-		return;
-	}
-	printf("%c", (char)tks.ty);
+	va_list ap;
+	va_start(ap, fmt);
+	
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, "");
+	fprintf(stderr, "^ ");
+	fprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
 }
 
-Node *new_node(int op, Node *lhs, Node *rhs)
+bool consume(char op)
 {
-	Node *node = (Node *)malloc(sizeof(Node));
-	node->op = op;
+	if(token->kind != TK_RESERVED || token->str[0] != op)
+		return false;
+	token = token->next;
+	return true;
+}
+
+//次のトークンが期待している記号のときにはトークンを１つ読み進める
+//それ以外はエラー報告
+void expect(char op)
+{
+	if(token->kind != TK_RESERVED || token->str[0] != op)
+		error_at(token->str, "expected '%c'", op);
+	token = token->next;
+}
+
+//次のトークンが数値の場合、トークンを１つ読み進めてその数値を返す。
+//それ以外の場合はエラーを報告する
+int expect_number()
+{
+	if(token->kind != TK_NUM)
+		error_at(token->str, "expected a number");
+	int val = token->val;
+	token = token->next;
+	return val;
+}
+
+bool at_eof()
+{
+	return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
+	Token *tok = calloc(1, sizeof(Token));
+	tok->kind = kind;
+	tok->str = str;
+	cur->next = tok;
+	return tok;
+}
+
+Node *new_node(NodeKind kind)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
+{
+	Node *node = new_node(kind);
 	node->lhs = lhs;
 	node->rhs = rhs;
 	return node;
 }
 
-Node *new_node_num(int val){
-	Node *node = (Node *)malloc(sizeof(Node));
-	node->ty = ND_NUM;
-	node->op = ND_NUM;
+Node *new_num(int val){
+	Node *node = new_node(ND_NUM);
 	node->val = val;
 	return node;
 }
+Node *expr();
+Node *mul();
+Node *unary();
+Node *primary();
 
-Node *add()
+Node *expr()
 {
 	Node *node = mul();
-
-    for(;;){
-        if(consume('+'))
-            node = new_node('+', node, mul());
-        else if(consume('-'))
-            node = new_node('-', node, mul());
-        else return node;
-    }
+	
+	for(;;){
+		if(consume('+'))
+			node = new_binary(ND_ADD,  node, mul());
+		else if(consume('-'))
+			node = new_binary(ND_SUB, node, mul());
+		else
+			return node;		
+	}
 }
 
 Node *mul()
 {
-	Node *node = term();
+	Node *node = unary();
 
-    for(;;){
-        if(consume('*'))
-            node = new_node('*', node, term());
-        else if(consume('/'))
-            node = new_node('/', node, term());
-        else return node;
-    }
-
-	printf("mul_out");
-	error("想定しないトークンです : %s", tokens[pos].input);
+	for(;;){
+		if(consume('*'))
+			node = new_binary(ND_MUL, node, unary());
+		else if(consume('/'))
+			node = new_binary(ND_DIV, node, unary());
+		else return node;
+	}
 }
 
-Node *term()
+Node *unary()
 {
-	//if(tokens[pos].ty == TK_NUM)
-	//	return new_node_num(tokens[pos++].val);
-	
+	if(consume('+'))
+		return unary();
+	if(consume('-'))
+		return new_binary(ND_SUB, new_num(0), unary());
+	return primary();
+}
+
+Node *primary()
+{
 	if(consume('(')){
-        Node *node = add();
-        if(!consume(')'))
-            error("開き括弧に対応する閉じ括弧がありません: %s", tokens[pos].input);
-        return node;
-    }
-    
-    if(tokens[pos].ty == TK_NUM)
-        return new_node_num(tokens[pos++].val);
-
-	error("数値でも開きカッコでもないトークンです : %s", tokens[pos].input);
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	return new_num(expect_number());
 }
 
-int consume(int ty)
+Token *tokenize()
 {
-	if(tokens[pos].ty != ty)
-		return 0;
-	pos++;
-	return 1;
+	char *p = user_input;
+	Token head;
+	head.next = NULL;
+	Token *cur = &head;
+
+#ifdef debug
+	int c = 0;
+#endif
+
+	while(*p){
+		if(isspace(*p)){
+			p++;
+			continue;
+		}
+		if(strchr("+-*/()", *p)){
+			cur = new_token(TK_RESERVED, cur, p++);
+#ifdef debug
+			printf("%d", ++c);
+#endif
+			continue;
+		}
+
+		if(isdigit(*p)){
+			cur = new_token(TK_NUM, cur, p);
+			cur->val = strtol(p, &p, 10);
+			continue;
+		}
+
+		error_at(p, "invalid token");
+	}
+	new_token(TK_EOF, cur, p);
+	return head.next;
 }
 
 void gen(Node *node)
 {
-	if(node->op == ND_NUM){
+	if(node->kind == ND_NUM){
 		printf("	push %d\n", node->val);
 		return;
 	}
-	if(node->lhs != NULL){
-		gen(node->lhs);
-	}
-	if(node->rhs != NULL){
-		gen(node->rhs);
-	}
+	gen(node->lhs);
+	gen(node->rhs);
 	
 	printf("	pop rdi\n");
 	printf("	pop rax\n");
 
-	switch(node->op){
-		case '+':
+	switch(node->kind){
+		case ND_ADD:
 			printf("	add rax, rdi\n");
 			break;
-		case '-':
+		case ND_SUB:
 			printf("	sub rax, rdi\n");
 			break;
-		case '*':
+		case ND_MUL:
 			printf("	imul rax, rdi\n");
 			break;
-		case '/':
-			printf("	div rax, rdi\n");
+		case ND_DIV:
+			printf(" 	cqo\n");
+			printf("	idiv rdi\n");
 			break;
 	}
 
@@ -236,16 +254,9 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-
-	if(!strcmp(argv[1], "-test")){
-		runtest();
-		return 0;
-	}
-
-//	printf("args : %s\n", argv[1]);
-
-	tokennize(argv[1]);
-	Node *node = add();
+	user_input = argv[1];
+	token = tokenize(user_input);
+	Node *node = expr();
 //	printf("\n");
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
